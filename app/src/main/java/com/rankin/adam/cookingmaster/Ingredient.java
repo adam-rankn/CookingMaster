b58a18b05dbdf6a4a8edfd2539ef5a9e61afcde5
adam <arankin@ualberta.ca>
2018-06-26 13:04:22 -0600

    Initial commit

---
+++ app/src/main/java/com/rankin/adam/cookingmaster/Ingredient.java
@@ -0,0 +1,8 @@
+package com.rankin.adam.cookingmaster;
+
+/**
+ * Created by Adam on 26-Jun-18.
+ */
+
+public class Ingredient {
+}
---
+++ app/src/main/java/com/rankin/adam/cookingmaster/Recipe.java
@@ -0,0 +1,38 @@
+package com.rankin.adam.cookingmaster;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Adam on 26-Jun-18.
+ */
+
+public class Recipe {
+
+    private String Name;
+    private String Time;
+    private ArrayList<Ingredient> IngredientList;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getTime() {
+        return Time;
+    }
+
+    public void setTime(String time) {
+        Time = time;
+    }
+
+    public ArrayList<Ingredient> getIngredientList() {
+        return IngredientList;
+    }
+
+    public void setIngredientList(ArrayList<Ingredient> ingredientList) {
+        IngredientList = ingredientList;
+    }
+}
