d64b99eb28c3ecc79b8e355ae3351b0381bd5278
adam <arankin@ualberta.ca>
2018-07-17 20:55:33 -0600

    comment out for crash fixc

--- app/src/main/java/com/rankin/adam/cookingmaster/activity/RecipeBookActivity.java
+++ app/src/main/java/com/rankin/adam/cookingmaster/activity/RecipeBookActivity.java
@@ -37,7 +37,7 @@ public class RecipeBookActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_book);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
 
         adapterList.addAll(recipeList.getRecipeList());
 
