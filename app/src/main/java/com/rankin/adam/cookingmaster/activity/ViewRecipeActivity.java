bc9d3aa5a490fa7e3aa0288cba3b97fb178ec220
adam <arankin@ualberta.ca>
2018-07-22 14:37:33 -0600

    gradle update

--- app/src/main/java/com/rankin/adam/cookingmaster/activity/ViewRecipeActivity.java
+++ app/src/main/java/com/rankin/adam/cookingmaster/activity/ViewRecipeActivity.java
@@ -61,7 +61,6 @@ public class ViewRecipeActivity extends AppCompatActivity {
         viewAllergensButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO show the allergens
                 ArrayList<String> allergenList = recipeController.getAllergens();
                 int count = allergenList.size();
                 final CharSequence[] dialogList =  allergenList.toArray(new CharSequence[count]);
