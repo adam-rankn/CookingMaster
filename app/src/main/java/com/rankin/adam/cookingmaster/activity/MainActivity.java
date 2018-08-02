5e68abf5760483df88ba402ba85b6099b880d901
adam <arankin@ualberta.ca>
2018-08-01 19:42:16 -0600

    threading for svaeload

--- app/src/main/java/com/rankin/adam/cookingmaster/activity/MainActivity.java
+++ app/src/main/java/com/rankin/adam/cookingmaster/activity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.rankin.adam.cookingmaster.activity;
 
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -28,9 +30,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         if (recipeList.getSize() == 0) {
-            saveLoadController.loadRecipesFromFile();
+            new Handler(Looper.getMainLooper()).post(new Runnable() {
+                public void run() {
+                    saveLoadController.loadRecipesFromFile();
+                }
+            });
         }
-        saveLoadController.loadShoppingListFromFile();
+
+        if (shoppingListController.getSize() == 0) {
+            new Handler(Looper.getMainLooper()).post(new Runnable() {
+                public void run() {
+                    saveLoadController.loadShoppingListFromFile();
+                }
+            });
+        }
 
         Button recipesButton = findViewById(R.id.btn_recipe_book);
         recipesButton.setOnClickListener(new View.OnClickListener() {
